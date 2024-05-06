dc322388c9466d8d757898db7e77ed3b0345fbf1
Alnb87 <camilaasly04@gmail.com>
2024-05-05 23:47:43 -0500

    Vista Inversionista

--- src/main/java/com/crud/proyecto/entity/Usuario.java
+++ src/main/java/com/crud/proyecto/entity/Usuario.java
@@ -7,7 +7,11 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
 @Entity
 @Table(name = "usuarios")
 public class Usuario {
@@ -22,87 +26,13 @@ public class Usuario {
     private String telefono;
     private String correo;
     private String username;
+    
+    private int dni;
 
     @ManyToOne
     @JoinColumn(name = "rol_id", referencedColumnName = "id")
     private Rol rol;
 
-    public Usuario() {
-    }
-
-    public Usuario(String nombre, String apellidos, String contrasena, String telefono, String correo, String username, Rol rol) {
-        this.nombre = nombre;
-        this.apellidos = apellidos;
-        this.contrasena = contrasena;
-        this.telefono = telefono;
-        this.correo = correo;
-        this.username = username;
-        this.rol = rol;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public String getApellidos() {
-        return apellidos;
-    }
-
-    public void setApellidos(String apellidos) {
-        this.apellidos = apellidos;
-    }
-
-    public String getContrasena() {
-        return contrasena;
-    }
-
-    public void setContrasena(String contrasena) {
-        this.contrasena = contrasena;
-    }
-
-    public String getTelefono() {
-        return telefono;
-    }
-
-    public void setTelefono(String telefono) {
-        this.telefono = telefono;
-    }
-
-    public String getCorreo() {
-        return correo;
-    }
-
-    public void setCorreo(String correo) {
-        this.correo = correo;
-    }
-
-    public Rol getRol() {
-        return rol;
-    }
-
-    public void setRol(Rol rol) {
-        this.rol = rol;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
 
 	@Override
 	public String toString() {
